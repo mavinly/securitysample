2142adfa7fc84b206011710832aedb3a7a7d7eae
unknown <CMURPHYADM@PC04016.hdna.hd.lan>
2016-01-05 15:26:39 -0700

    Initial check in

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var passport = require('passport');
+var User = mongoose.model('User');
+var router = express.Router();
+var jwt = require('express-jwt');
+
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
+
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
+});
+
+module.exports = router;
