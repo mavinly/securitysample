6b689ba1c4a8e35935ece6bd33020f1496e56620
unknown <cmurphy@PC04016.hdna.hd.lan>
2016-01-06 15:54:45 -0700

    Security updates

--- public/javascripts/securityApp.js
+++ public/javascripts/securityApp.js
@@ -40,4 +40,68 @@ app.factory('auth', ['$http', '$window', '$rootScope', function($http, $window, 
     logOut: function(){
       $window.localStorage.removeItem('sampleSecurity-token');
     }
+   };
+   return auth;
+  }]);
+  
+  app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
   };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}])
+  
+  app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl',
+      resolve: {
+        Promise: ['readings', function(readings){
+          return readings.getAll();
+        }]
+      }
+    })
+    
+    .state('login', {
+      url: '/login',
+      templateUrl: '/login.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+        }
+      }]
+    })
+    
+  .state('register', {
+      url: '/register',
+      templateUrl: '/register.html',
+      controller: 'AuthCtrl',
+      onEnter: ['$state', 'auth', function($state, auth){
+        if(auth.isLoggedIn()){
+          $state.go('home');
+        }
+      }]
+    });
+
+  $urlRouterProvider.otherwise('home');
+}]);
