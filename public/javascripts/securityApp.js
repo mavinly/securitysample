a3099c8144d724c6964c5ea0e0d2fcadabd100e0
unknown <cmurphy@PC04016.hdna.hd.lan>
2016-01-07 15:57:03 -0700

    Security Changes

--- public/javascripts/securityApp.js
+++ public/javascripts/securityApp.js
@@ -1,107 +1,109 @@
 var app = angular.module('securitySample', ['ui.router']);
 
-app.factory('auth', ['$http', '$window', '$rootScope', function($http, $window, $rootScope){
-   var auth = {
-    saveToken: function (token){
-      $window.localStorage['sampleSecurity-token'] = token;
-    },
-    getToken: function (){
-      return $window.localStorage['sampleSecurity-token'];
-    },
-    isLoggedIn: function(){
-      var token = auth.getToken();
+app.factory('auth', ['$http', '$window', '$rootScope', function ($http, $window, $rootScope) {
+    var auth = {
+        saveToken: function (token) {
+            $window.localStorage['sampleSecurity-token'] = token;
+        },
+        getToken: function () {
+            return $window.localStorage['sampleSecurity-token'];
+        },
+        isLoggedIn: function () {
+            var token = auth.getToken();
 
-      if(token){
-        var payload = JSON.parse($window.atob(token.split('.')[1]));
+            if (token) {
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.exp > Date.now() / 1000;
+            } else {
+                return false;
+            }
+        },
+        currentUser: function () {
+            if (auth.isLoggedIn()) {
+                var token = auth.getToken();
+                var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+                return payload.username;
+            }
+        },
+        register: function (user) {
+            return $http.post('/register', user).success(function (data) {
+                auth.saveToken(data.token);
+            });
+        },
+        logIn: function (user) {
+            return $http.post('/login', user).success(function (data) {
+                auth.saveToken(data.token);
+            });
+        },
+        logOut: function () {
+            $window.localStorage.removeItem('sampleSecurity-token');
+        }
+    };
+    return auth;
+}]);
+
+app.controller('AuthCtrl', [
+    '$scope',
+    '$state',
+    'auth',
+    function ($scope, $state, auth) {
+        $scope.user = {};
+        $scope.currentUser = auth.currentUser;
+        $scope.logOut = auth.logOut;
         
-        return payload.exp > Date.now() / 1000;
-      } else {
-        return false;
-      }
-    },
-    currentUser: function(){
-      if(auth.isLoggedIn()){
-        var token = auth.getToken();
-        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        $scope.register = function () {
+            auth.register($scope.user).error(function (error) {
+                $scope.error = error;
+            }).then(function () {
+                $state.go('home');
+            });
+        };
 
-        return payload.username;
-      }
-    },
-    register: function(user){
-      return $http.post('/register', user).success(function(data){
-        auth.saveToken(data.token);
-      });
-    },
-    logIn: function(user){
-      return $http.post('/login', user).success(function(data){
-        auth.saveToken(data.token);
-      });
-    },
-    logOut: function(){
-      $window.localStorage.removeItem('sampleSecurity-token');
-    }
-   };
-   return auth;
-  }]);
-  
-  app.controller('AuthCtrl', [
-'$scope',
-'$state',
-'auth',
-function($scope, $state, auth){
-  $scope.user = {};
+        $scope.logIn = function () {
+            auth.logIn($scope.user).error(function (error) {
+                $scope.error = error;
+            }).then(function () {                
+                $state.go('home');
+            });
+        };
+    }])
 
-  $scope.register = function(){
-    auth.register($scope.user).error(function(error){
-      $scope.error = error;
-    }).then(function(){
-      $state.go('home');
-    });
-  };
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'AuthCtrl',
+            resolve: {
+                Promise: ['readings', function (readings) {
+                    return readings.getAll();
+                }]
+            }
+        })
 
-  $scope.logIn = function(){
-    auth.logIn($scope.user).error(function(error){
-      $scope.error = error;
-    }).then(function(){
-      $state.go('home');
-    });
-  };
-}])
-  
-  app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/home',
-      templateUrl: '/home.html',
-      controller: 'MainCtrl',
-      resolve: {
-        Promise: ['readings', function(readings){
-          return readings.getAll();
-        }]
-      }
-    })
-    
-    .state('login', {
-      url: '/login',
-      templateUrl: '/login.html',
-      controller: 'AuthCtrl',
-      onEnter: ['$state', 'auth', function($state, auth){
-        if(auth.isLoggedIn()){
-          $state.go('home');
-        }
-      }]
-    })
-    
-  .state('register', {
-      url: '/register',
-      templateUrl: '/register.html',
-      controller: 'AuthCtrl',
-      onEnter: ['$state', 'auth', function($state, auth){
-        if(auth.isLoggedIn()){
-          $state.go('home');
-        }
-      }]
-    });
+        .state('login', {
+            url: '/login',
+            templateUrl: '/login.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'auth', function ($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go('home');
+                }
+            }]
+        })
 
-  $urlRouterProvider.otherwise('home');
+        .state('register', {
+            url: '/register',
+            templateUrl: '/register.html',
+            controller: 'AuthCtrl',
+            onEnter: ['$state', 'auth', function ($state, auth) {
+                if (auth.isLoggedIn()) {
+                    $state.go('home');
+                }
+            }]
+        });
+
+    $urlRouterProvider.otherwise('home');
 }]);
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.post('/login', function(req, res, next){
   if(!req.body.username || !req.body.password){
     return res.status(400).json({message: 'Please fill out all fields'});
   }
-
+  
   passport.authenticate('local', function(err, user, info){
     if(err){ return next(err); }
 
@@ -26,6 +26,24 @@ router.post('/login', function(req, res, next){
       return res.status(401).json(info);
     }
   })(req, res, next);
+});
+
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
+
+  var user = new User();
+
+  user.username = req.body.username;
+
+  user.setPassword(req.body.password)
+
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
 });
 
 module.exports = router;
